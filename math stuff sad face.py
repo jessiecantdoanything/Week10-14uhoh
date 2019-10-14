9d54ddc427d3f219f959c7741840a7f643805ebf
100148965 <canterojessie@gmail.com>
2019-10-14 15:34:57 -0500

    learning to calculate pi on python

---
+++ math stuff sad face.py
@@ -0,0 +1,23 @@
+# we are going to look at approximations of Pi
+# as well as looking at the math module
+
+print(22/7)
+print(355/113)
+
+import math
+
+print(9801 / (2206 * math.sqrt(2)))
+
+def archimedes(numSides):
+    innerAngleB = 360.0 / numSides
+    halfAngleA = innerAngleB / 2
+    oneHalfSideS = math.sin(math.radians(halfAngleA))
+    sideS = oneHalfSideS * 2
+    polygonCircumference = numSides * sideS
+    pi = polygonCircumference / 2
+    return pi
+
+print(archimedes(4))
+print(archimedes(9))
+print(archimedes(18))
+print(sides, archimedes(sides))
