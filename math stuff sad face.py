3f4a288cf9367a6fcc52aacf6057e65f0069350c
100148965 <canterojessie@gmail.com>
2019-10-15 15:55:49 -0500

    doing some math mr. black assigned us :OO a new day a new math problem yipee

--- math stuff sad face.py
+++ math stuff sad face.py
@@ -22,7 +22,48 @@ print(archimedes(9))
 print(archimedes(18))
 
 
-for sides in range(40, 400, 40):
+for sides in range(40000000, 400000000, 40000000):
     print(sides, archimedes(sides))
 
 print(math.pi)
+#360000000 is the number of sides that it takes to make pi
+print(archimedes(360000000))
+
+# accumulators
+# THIS IS NOT ALGEBRA JESSIE
+
+# 0 + 0 = 0
+# 0 + 1 = 1
+# 1 + 2 = 3
+# 3 + 3 = 6
+# 6 + 4 = 10
+# 10 + 5 = 15
+acc = 0
+for val in range (1, 5):
+    acc = acc + val
+print(acc)
+
+# Compute the sum of the first 100 even numbers
+
+acc = 0
+for val in range(0, 201, 2):
+    acc = acc + val
+print(acc)
+
+# Compute the sum of the first 50 odd numbers
+
+acc = 0
+for val in range(1, 101, 2):
+    acc = acc + val
+print(acc)
+
+# Compute the average of the first 100 odd numbers
+
+acc = 0
+for val in range(1, 201, 2):
+    acc = acc + val / 100
+print(acc)
+
+# Write a function that returns the average of the first N numbers, where N is a parameter
+
+
