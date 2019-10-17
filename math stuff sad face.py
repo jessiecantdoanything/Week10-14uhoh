5e0b4018df794d38a0b1f8f6991bc61162685d31
100148965 <canterojessie@gmail.com>
2019-10-17 15:23:35 -0500

    finished perhaps who knows

--- math stuff sad face.py
+++ math stuff sad face.py
@@ -65,5 +65,37 @@ for val in range(1, 201, 2):
 print(acc)
 
 # Write a function that returns the average of the first N numbers, where N is a parameter
+def averij(N):
+    acc = 0
+    for val in range(0, N, 1):
+        acc = acc + val
+    print(acc/N)
 
+print(averij(6))
+# Write a function called factorial that computes the product of the first N
+#   numbers, where N is a parameter
+def factorial(N):
+    acc = 1
+    for val in range(1, N+1, 1):
+        acc = acc * val
+    print(acc)
+
+print(factorial(6))
+
+# Each number in the Fibonacci sequence is the sum of the previous two numbers.
+#   The first two numbers in the sequence are 1 and 1.  Compute the 10th
+#   Fibonacci number.
+acc = 0
+for Fibonacci in range(1, 11,):
+    acc = acc + Fibonacci
+print(acc)
+
+# Write a function to compute the Nth Fibonacci number, where N is a parameter.
+#   You may assume that N will be greater than or equal to 3.
+def Fibonacci(N):
+    acc = 0
+    for Fibonacci in range(1, N):
+        acc = acc + Fibonacci
+    print(acc)
+print(Fibonacci(11))
 
